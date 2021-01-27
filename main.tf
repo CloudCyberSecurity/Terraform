f186f32cf8410d1f71c11e64b8eda366bb3de7e6
Imran <nationalcybersecure@gmail.com>
2021-01-27 16:26:04 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+resource "aws_instance" "MyFirstTerraformInstance" {
+ami             = "ami-0a6dc7529cd559185"
+instance_type   = "t2.micro"
+tags = {
+  "Name" = "MyTerraformEC2Instance1"
+}
+}
