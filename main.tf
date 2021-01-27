723fd7f188db4dcbb56ec988b3af5b64eb1d6d09
Imran <nationalcybersecure@gmail.com>
2021-01-27 16:58:29 +0000

    commit 2

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "MyFirstTerraformInstance" {
 ami             = "ami-0a6dc7529cd559185"
 instance_type   = "t2.micro"
 tags = {
-  "Name" = "MyTerraformEC2Instance1"
+  "Name" = "MyTerraformEC2Instance11"
 }
 }
