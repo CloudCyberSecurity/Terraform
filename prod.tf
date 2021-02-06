f659f4b4790c2f64a66ece29449b3364cfea4821
Imran <nationalcybersecure@gmail.com>
2021-02-06 17:19:40 +0000

    added loadbalancer

--- prod.tf
+++ prod.tf
@@ -6,9 +6,26 @@ provider "aws" {
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "imran-terraform-20210206"
   acl    = "private"
+  tags = {
+    "Terraform" = "true"
+  }
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+    availability_zone = "us-east-1a"
+    tags = {
+    "Terraform" = "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+    availability_zone = "us-east-1b"
+    tags = {
+    "Terraform" = "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -59,6 +76,24 @@ resource "aws_eip_association" "prod_web" {
 }
 resource "aws_eip" "prod_web" {
   tags          = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+    name            = "prod-web"
+    instances       = aws_instance.prod_web.*.id
+    subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    security_groups = [aws_security_group.prod_web.id]
+
+    listener {
+      instance_port     = 80
+      instance_protocol = "http"
+      lb_port           = 80
+      lb_protocol       = "http"
+      
+    }
+    tags = {
     "Terraform" : "true"
   }
 }
