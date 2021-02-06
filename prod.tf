f8c42bc7e709289755bb4bcdf6df1bfbe04b0bfa
Imran <nationalcybersecure@gmail.com>
2021-02-06 14:24:54 +0000

    Web instances and elastic ip

--- prod.tf
+++ prod.tf
@@ -37,3 +37,24 @@ tags = {
     "Terraform" : "true"
   }
 }
+
+resource "aws_instance" "prod_web" {
+  ami = "ami-068478a870bb53228"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [ 
+    aws_security_group.prod_web.id
+   ]
+
+tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip" "prod_web" {
+    instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
