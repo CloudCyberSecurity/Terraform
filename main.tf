029c000543053024d2c3c85d57154430a975058e
Imran <nationalcybersecure@gmail.com>
2021-02-06 09:51:47 +0000

    before resource change

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "MyTerraformInstanceACG-S3-BUCKET-TEST" {
+resource "aws_instance" "MyTerraformInstanceLinkedIn" {
 ami             = "ami-047a51fa27710816e"
 instance_type   = "t2.micro"
   key_name   = "nvirginia"
  
   tags = {
-    Name = "TACG2-EC2"
+    Name = "LinkedInLectures-EC2"
   }
 }
